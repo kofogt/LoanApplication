99be2cff5b667b5d7802c215b0f05e627ef3b6a8
Kofoworola George-Taylor <kofogt@gmail.com>
2022-07-17 06:00:10 +0100

    calls for new method

--- LoanApplication/Checker.java
+++ LoanApplication/Checker.java
@@ -2,9 +2,10 @@ package LoanApplication;
 
 public class Checker {
     public static void main(String[] args) {
-        LoanEligibility jane = new LoanEligibility(15000,18,false,true,false);
+        LoanEligibility jane = new LoanEligibility(15000,19,false,true,false);
        if (jane.isEligibleForLoan()){
            ApplyForLoan.getLoanDetails();
+           ApplyForLoan.takeLoan();
        }else{
            System.out.println("You are not eligible for a loan.");
        }
